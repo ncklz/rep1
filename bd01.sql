28f21e3ed6c502f4200201273013165d17c2cb13
ncklz <ncklzcp@gmail.com>
2017-01-17 03:49:11 -0500

    Add files via upload

---
+++ bd01.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE Persona
+(
+Idn int primary key,
+Nombre varchar(100),
+Apellido varchar(100))
+;
+
+insert into persona values (1,'nicolas','castillo');
+insert into persona values (2,'pedro','perez');
+insert into persona values (3,'mario','mendez');
+insert into persona values (4,'pablo','escobar');
+
